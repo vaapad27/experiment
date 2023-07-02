6a4e5605b64ec38882b6c0211df2ce4fad233b19
Long <lephamlong@gmail.com>
2023-07-02 16:53:54 +0700

    api phong

---
+++ dboperations.js
@@ -0,0 +1,39 @@
+var config = require('./dbconfig');
+const sql = require('mssql');
+
+
+async function datPhong(){
+    try{
+        let pool = await sql.connect(config);
+        let products = await pool.request().query("SELECT * from PhieuDatPhong");
+        return (await products).recordsets;
+    }
+    catch (error){
+        console.log(error);
+
+    }
+}
+
+
+//room booking đến db
+async function addPhieuDatPhong(phieuDatPhong) {
+    try {
+        let pool = await sql.connect(config);
+        let query =
+            `INSERT INTO PhieuDatPhong (MaPhieu, TenKhachHang, SDT, Email, DiaChi, SoNguoiLon, SoTreEm, NgayDenNhan, NgayTra, MaLoaiPhong, SoLuongPhong, MaDichVu, GhiChu)
+            VALUES ('${phieuDatPhong.MaPhieu}', '${phieuDatPhong.TenKhachHang}', '${phieuDatPhong.SDT}', '${phieuDatPhong.Email}', '${phieuDatPhong.DiaChi}',
+            '${phieuDatPhong.SoNguoiLon}', '${phieuDatPhong.SoTreEm}', '${phieuDatPhong.NgayDenNhan}', '${phieuDatPhong.NgayTra}', '${phieuDatPhong.MaLoaiPhong}',
+            '${phieuDatPhong.SoLuongPhong}', '${phieuDatPhong.MaDichVu}', '${phieuDatPhong.GhiChu}')`;
+
+        let result = await pool.request().query(query);
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+//export module
+module.exports ={
+datPhong : datPhong,
+addPhieuDatPhong: addPhieuDatPhong
+}
